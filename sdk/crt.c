355b21b895984fd948bdb58d221f6c5d9a220531
Manuel Spagnolo <manuel.spagnolo@proton.me>
2026-03-01 22:48:30 +0100

    refactor: rename C runtime methods to not clash with known ones
    The existing names happen to clash with names in linux and other OS's
    which get picked up by diagnostic tools. This change removes those
    warnings using names that are unique to OSle.

--- sdk/crt.c
+++ sdk/crt.c
@@ -1,22 +1,20 @@
 extern int main(void);
 
 __attribute__((naked, section(".text.epilogue"), noreturn)) 
-void _exit(void) {
+void __epilogue(void) {
   __asm__ volatile("int $0x20" :::);
 }
 
-__attribute__((naked, section(".text.prologue"), noreturn)) 
-void _start(void) {
+__attribute__((section(".text.prologue"), noreturn)) 
+void __prologue(void) {
   __asm__ volatile(
-    // Set video mode
-    "mov $0x00, %%ah\n"
-    "mov $0x03, %%al\n"
+    "mov $0x0003, %%ax\n"
     "int $0x10"
     ::: "ax"
   );
 
   main();
 
-  _exit();
+  __epilogue();
 }
 
